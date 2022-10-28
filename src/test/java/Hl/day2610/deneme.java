d53bd64171fc922055f7fda8cc5fa05f882ebaa3
Hldka <116027001+Hldka@users.noreply.github.com>
2022-10-28 12:37:55 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ src/test/java/Hl/day2610/deneme.java
@@ -0,0 +1,14 @@
+package Hl.day2610;
+
+public class deneme {public static void main(String[] args) { add(3.2, 4.8); //Output A
+    add(5, 7.3); //Output B
+     add(5, 7);// Output C
+    add(5.3, 7); //Output D
+}
+    public static void add(double n1, double n2) { System.out.println(n1 + n2);
+    }
+    public static void add(int n1, double n2) {
+        System.out.println(n1 + n2); }
+    public static void add(int n1, int n2) { System.out.println(n1 + n2);
+    }
+}
