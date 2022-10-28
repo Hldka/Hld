cc5f11ee92082c055df9da0e84a7e60c0c5e3090
Hldka <116027001+Hldka@users.noreply.github.com>
2022-10-28 12:38:05 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ src/test/java/Hl/day2710/practiceQ01.java
@@ -0,0 +1,21 @@
+package Hl.day2710;
+
+public class practiceQ01 {/*
+   Toplamları, verilen bir sayıya eşit olan  Array element çiflerini yazdıran bir method oluşturunuz
+   {4,6,5,-10,8,5,20} ===> 10
+   4 + 6 = 10
+   5 + 5 = 10
+  -10 + 20 = 10
+   */
+    public static void main(String[] args) {
+int []arr={4,6,5,-10,8,5,20};
+ikilileribul(arr,10);
+
+
+    } public static void ikilileribul (int []arr,int number){
+        for (int i=0;i< arr.length;i++){
+
+        }
+
+    }
+}
