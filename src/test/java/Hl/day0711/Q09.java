105e9a25b7a252e084ea9bf4aee769063c7215a5
Hldka <116027001+Hldka@users.noreply.github.com>
2022-11-08 11:35:50 +0100

    Merge remote-tracking branch 'origin/main'

--- src/test/java/Hl/day0711/Q09.java
+++ src/test/java/Hl/day0711/Q09.java
@@ -1,4 +1,11 @@
 package Hl.day0711;
 
 public class Q09 {
+    /*
+    9)Bir String değişkeni oluşturunuz, String'deki rakam olmayan karakterlerin sayısını konsolda yazdırınız.
+Örnek: String: '1a3Bcf4!...' ise konsolda 8 yazdırmanız gerekir.
+     */
+    public static void main(String[] args) {
+        String str="1a3Bcf4!...";
+    }
 }
---
+++ src/test/java/Hl/day0811/Questions.java
@@ -0,0 +1,22 @@
+package Hl.day0811;
+
+public class Questions {
+    /*
+    2×2'lik bir ızgaranın sol üst köşesinden başlayarak ve yalnızca sağa ve aşağı hareket edebilen,
+
+sağ alt köşeye giden tam 6 yol vardır.
+
+
+
+
+20 × 20’lik ızgarada böyle kaç rota var?
+
+
+16.Soru
+
+
+2¹⁵ = 32768 ve rakamları toplamı 3 + 2 + 7 + 6 + 8 = 26'dır.
+
+ 2¹⁰⁰⁰ sayısının rakamları toplamı kaçtır?
+     */
+}
