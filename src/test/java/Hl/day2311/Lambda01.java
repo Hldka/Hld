cfa559601abe549a52741eebc3f1dab4db51d16f
Hldka <116027001+Hldka@users.noreply.github.com>
2022-11-23 10:31:44 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/test/java/Hl/day2311/Lambda01.java
@@ -0,0 +1,11 @@
+package Hl.day2311;
+
+public class Lambda01 {
+    /*
+    1) absolute path seciyoruz yada
+    2) path form content root seciyoruz
+    her ikisinde de path kopyalamis oluyoruz
+    cevirirken EXCEPTION OLUSMASIN DIYE ==> throws IOException <= EKLEMEYIZ
+    buda lines altinda kirmizi
+     */
+}
