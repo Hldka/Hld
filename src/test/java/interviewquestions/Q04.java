65e6f63cee5dda0af39e1766b61b1903a5218c3b
Hldka <116027001+Hldka@users.noreply.github.com>
2022-12-03 13:41:15 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/test/java/interviewquestions/Q04.java
@@ -0,0 +1,4 @@
+package interviewquestions;
+
+public class Q04 {//Asal olup olmadığını kontrol eden method yaziniz
+}
---
+++ src/test/java/interviewquestions/Q05.java
@@ -0,0 +1,10 @@
+package interviewquestions;
+
+public class Q05 {/*
+Bir Array oluşturun ve
+en büyük ve en küçük sayılar arasındaki farkı veririn.
+Kullanıcıdan Array  öğelerini girmesini isteyin.*/
+
+
+
+}
