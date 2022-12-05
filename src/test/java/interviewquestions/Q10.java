90a1772c0d95d3040d89f4a291fbef47c2a19991
Hldka <116027001+Hldka@users.noreply.github.com>
2022-12-05 09:36:18 +0100

    Merge remote-tracking branch 'origin/main'

--- src/test/java/interviewquestions/Q10.java
+++ src/test/java/interviewquestions/Q10.java
@@ -19,7 +19,36 @@ String is "Ali came to school and Ayse came to school"
         for (String w:str1) {  if (w==w){count++;}else System.out.println(w+"= 1 ");
 
         }
+/*public static void main(String[] args) {
 
+		String str = "Ali came to school, and Ayse came to school.";
+		String words[] = str.split(" ");
+		System.out.println(Arrays.toString(words));
+
+		List<String> wordsList = new ArrayList<String>();
+		for(String w : words) {
+			wordsList.add(w.replaceAll("\\W", ""));
+		}
+		System.out.println(wordsList);// [Ali, came, to, school, and, Ayse, came, to, school]
+
+		Map<String, Integer> numOfWords = new HashMap<>();// {                                   }
+
+		for(String w : wordsList) {
+
+			if(numOfWords.containsKey(w)) {
+
+				numOfWords.put(w, numOfWords.get(w)+1);
+
+			}else {
+
+				numOfWords.put(w,1);
+
+			}
+		}
+		System.out.println(numOfWords);
+
+	}
+*/
     }
 
 }
--- src/test/java/interviewquestions/Q12.java
+++ src/test/java/interviewquestions/Q12.java
@@ -12,5 +12,36 @@ public class Q12 {
      * Fonksiyonel Programlama
      *
      */
+/*public static void main(String[] args) {
 
+		int a[] = { 0, 2, 5, 3, 1 };
+		List<Integer> list = new ArrayList<>();
+		Arrays.stream(a).forEach(t -> list.add(t));
+
+		int max = list.stream().reduce(Integer.MIN_VALUE, Integer::max);
+
+		int idxOfMax = list.indexOf(max);
+
+		List<Integer> part1 = new ArrayList<>();
+		list.stream().filter(t -> list.indexOf(t) <= idxOfMax).forEach(t -> part1.add(t));
+
+		List<Integer> sortedPart1 = new ArrayList<>();
+		part1.stream().forEach(t -> sortedPart1.add(t));
+		Collections.sort(sortedPart1);
+
+		List<Integer> part2 = new ArrayList<>();
+		list.stream().filter(t -> list.indexOf(t) >= idxOfMax).forEach(t -> part2.add(t));
+
+		List<Integer> reverseSortedPart2 = new ArrayList<>();
+		part2.stream().forEach(t -> reverseSortedPart2.add(t));
+		Collections.sort(reverseSortedPart2);
+		Collections.reverse(reverseSortedPart2);
+
+		if (part1.equals(sortedPart1) && part2.equals(reverseSortedPart2)) {
+			System.out.println(list + " is Mountain Array");
+		} else {
+			System.out.println(list + " is not Mountain Array");
+		}
+	}
+*/
 }
