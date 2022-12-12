c8f0459be99de02ac5fa1528f614d604ddb4f13e
Hldka <116027001+Hldka@users.noreply.github.com>
2022-12-12 09:20:11 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/test/java/interviewquestions/Q34.java
@@ -0,0 +1,15 @@
+package interviewquestions;
+
+public class Q34 {
+
+    //Herhangi bir tamsayı kullanmadan 0'dan 100'e kadar tamsayıları yazdıran bir program yazın
+    public static void main(String[] args) {
+
+        for(char c=' '; c<'e'; c++) {
+
+            System.out.println((int)c);
+
+        }
+
+    }
+}
